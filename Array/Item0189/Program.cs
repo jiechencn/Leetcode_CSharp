61df215a4f19d66ff79534336722b74979fd12c7
Jie Chen <jiechencn@qq.com>
2024-12-04 22:54:43 +0800

    202241204-3

--- Array/Item0189/Program.cs
+++ Array/Item0189/Program.cs
@@ -8,15 +8,22 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
         int k = 3;
         new Program().Rotate(nums, k);
         nums.PrintAll();
     }
 
+    /// <summary>
+    /// Time: O(n)
+    /// Space: O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
     public void Rotate(int[] nums, int k)
     {
         int len = nums.Length;
+        k = k % len;
         Reverse(nums, 0, len - 1);
         Reverse(nums, 0, k - 1);
         Reverse(nums, k , len - 1);
