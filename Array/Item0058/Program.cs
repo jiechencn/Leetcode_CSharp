66c3879716a12220c6051c7bb0314a3164fb4db9
jiechencn <jiechencn@qq.com>
2025-01-15 08:33:29 +0800

    length-of-last-word

---
+++ Array/Item0058/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Item0058;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine(new Program().LengthOfLastWord("a bb ccc dddd"));
+        Console.WriteLine(new Program().LengthOfLastWord("a bb ccc dddd "));
+        Console.WriteLine(new Program().LengthOfLastWord(" a bb ccc dddd"));
+        Console.WriteLine(new Program().LengthOfLastWord(" a bb ccc dddd "));
+        Console.WriteLine(new Program().LengthOfLastWord(" a   bb   ccc  dddd  "));
+        Console.WriteLine(new Program().LengthOfLastWord(" a   bb   ccc   dddd    "));
+    }
+    
+    public int LengthOfLastWord(string s)
+    {
+        int index = s.Length - 1;
+        int wordLength = 0;
+        char[] chars = s.ToCharArray();
+        while (index>=0 && chars[index] == ' ')
+        {
+            index--;
+        }
+        
+        while (index>=0 && chars[index] != ' ')
+        {
+            wordLength++;
+            index--;
+        }
+
+        return wordLength;
+    }
+}
